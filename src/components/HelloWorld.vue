0269518e1c171a8c14fc4c7c326289ad57e95f09
Mokhles.Jaziri <Mokhles.Jaziri@cynapsys.local>
2022-09-16 15:30:36 +0100

    xossss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentat</a
+        >vue-cli documenssstat</a
       >.
     </p>
     <h3>Installed CLI Plsss</h3>
